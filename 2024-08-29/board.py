3a42eb1e2baa781acbad424e0f8f1ba612688631
Galo <nebulafa@gmail.com>
2024-08-30 04:56:24 -0300

    AAAAAAAAAAAAAAAA ODIO TODO

--- 2024-08-29/board.py
+++ 2024-08-29/board.py
@@ -4,5 +4,26 @@ class Board():
     """ Clase que representa al tablero de juego,
     guarda su estado y sus dimensiones. """
 
-    def __init__(self):
-        pass
+    def __init__(self, dimensions):
+        self.__dimensions = dimensions
+
+    def draw_initial_board(self):
+        """ Método que se encarga de dibujar el tablero inicial vacío """
+        board = []
+        for i in range(self.__dimensions):
+            for t in range(self.__dimensions):
+                board.append("▢")
+
+    def draw_board(self):
+        """ Método que se encarga de dibujar el tablero """
+        board = Board(3)
+        board.draw_initial_board()
+
+        for i in range(0, self.__dimensions * self.__dimensions, 3):
+            print(board[i] + "  | " + board[i + 1] + "  | " + board[i + 2]) #type: ignore
+
+        return board
+
+b = Board(3)
+
+b.draw_board()
--- 2024-08-29/test_tictactoe.py
+++ 2024-08-29/test_tictactoe.py
@@ -6,12 +6,21 @@ from tile import Tile
 from player import Player
 from checker import Checker
 
-class TestJuego(unittest.TestCase):
+class TestGame(unittest.TestCase):
     
     def test_01_instancia(self):
         game = Game()
 
         self.assertIsInstance(game, Game)
+
+class TestBoard(unittest.TestCase):
+
+    def test_01_dibjuar_tablero(self):
+        board = Board(3)
+
+        drawn_board = board.draw_board()
+
+        self.assertEqual(['▢', '▢', '▢', '▢', '▢', '▢', '▢', '▢', '▢'], drawn_board)
 
 if __name__ == "__main__":
     unittest.main()
